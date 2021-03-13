a6fcf5d85dd4886363ce85a11b190d636298e744
AlejandroHernandez <alejandro@sfleet.com>
2021-03-13 16:12:47 -0600

    task for create account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_040729) do
+ActiveRecord::Schema.define(version: 2021_03_13_215127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 2021_03_13_040729) do
     t.string "var_name"
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "service_status_catalogues", force: :cascade do |t|
+    t.string "name"
+    t.string "var_name"
+    t.string "status_equivalences"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- lib/tasks/migrate_data.rake
+++ lib/tasks/migrate_data.rake
@@ -2,16 +2,17 @@ namespace :migrate do
   
   desc "create providers"
   task create_provider: :enviroment do
-    if DeliveryServiceCatalogue.create(name: "FeDex", var_name: "fedex")
-      puts "create provider Fedex"
+    if DeliveryServiceCatalogue.create(name: 'FeDex', var_name: 'fedex', description: 'Servicio de paqueteria' )
+      puts 'create provider Fedex'
+      if DeliveryServiceAccount.create(name: 'Cuenta de challenge')
+        puts 'Creacion de cuenta del challenge'
+      end
     end
   end
 
   desc "create attributers for provider"
   task create_attribute: :enviroment do
-    option = {
-      []
-    }
+    option = {}
 
   end
 
