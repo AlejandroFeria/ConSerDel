85ab756a3263598475924c4f009ab3f4ac80b1b9
AlejandroHernandez <alejandro@sfleet.com>
2021-03-13 19:31:19 -0600

    add delivery_service_catalogues :lipstick:

--- app/models/delivery_service_catalogue.rb
+++ app/models/delivery_service_catalogue.rb
@@ -1,4 +1,6 @@
 class DeliveryServiceCatalogue < ApplicationRecord
   has_many :delivery_service_auth_keys
   has_many :delivery_service_accounts
+
+  accepts_nested_attributes_for :delivery_service_auth_keys, allow_destroy: true
 end
